73b6c99d2b64e08307b42fda751d0535aa44222f
LastOne <k.laszczych94@gmail.com>
2018-11-28 21:50:03 +0100

    Edited Asteroids

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -1,12 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Asteroid : MonoBehaviour
 {
+    private RectTransform rt;
+    private Transform canvas;
+
+    private void OnEnable()
+    {
+        canvas = transform.GetChild(0);
+        Text text = canvas.GetChild(0).GetComponent<Text>();
+        rt = canvas.GetComponent<RectTransform>();
+        text.text = Random.Range(0, 10).ToString();
+        canvas.SetParent(null);
+    }
 
     private void Update()
     {
+        rt.position = new Vector3(transform.position.x, 1, transform.position.z);
+
         if (System.Math.Abs(this.gameObject.transform.position.y) > 1)
         {
             this.gameObject.transform.position = new Vector3(transform.position.x, 0, transform.position.z);
@@ -28,8 +42,8 @@ public class Asteroid : MonoBehaviour
 
         if (this.gameObject.tag == "BigAsteroid")
         {
-            GameObject asteroid = DefaultPrefabs.Instance.GetRandomMediumAsteroid();
-            AsteroidGenerator.Instance.Shatter(asteroid, transform.position, 2);
+           // GameObject asteroid = DefaultPrefabs.Instance.GetRandomMediumAsteroid();
+            //AsteroidGenerator.Instance.Shatter(asteroid, transform.position, 2);
             if (other.gameObject.tag == "Bullet")
             {
                 ScoreManager.Instance.Score += 1;
@@ -38,32 +52,24 @@ public class Asteroid : MonoBehaviour
         }
         else if (this.gameObject.tag == "MediumAsteroid")
         {
-            GameObject asteroid = DefaultPrefabs.Instance.GetRandomSmallAsteroid();
-            AsteroidGenerator.Instance.Shatter(asteroid, transform.position, 2);
-            if (other.gameObject.tag == "Bullet")
-            {
-                ScoreManager.Instance.Score += 2;
-            }
+ 
         }
         else if (this.gameObject.tag == "SmallAsteroid")
         {
-            if (other.gameObject.tag == "Bullet")
-            {
-                BuffGenerator.Instance.InstantiateBuff(transform.position);
-                ScoreManager.Instance.Score += 3;
-            }
 
         }
         if (!other.CompareTag("Enemy"))
         {
             Instantiate(DefaultPrefabs.Instance.AsteroidExplosionVFX, transform.position, transform.rotation);
             Destroy(this.gameObject);
+            Destroy(canvas.gameObject);
         }
 
     }
     private void OnBecameInvisible()
     {
         Destroy(gameObject);
+        Destroy(canvas.gameObject);
     }
     public void InstantiateExplosionParticle()
     {
